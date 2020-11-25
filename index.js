51d752f92338493f619dd214cd594d05ab4492d0
bnicolas <nicolas.bremme@epitech.eu>
2020-11-25 14:34:39 +0100

    add

--- index.js
+++ index.js
@@ -153,6 +153,7 @@ app.post('/webhook/', function (req, res)
         }
 
         console.log(allUsers[sender]);
+        console.log(allUsers[sender].urlEntered);
         if (allUsers[sender].skip > 1) {
             allUsers[sender].skip--;
         }
