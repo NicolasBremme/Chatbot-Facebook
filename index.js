8335e15268a0e76705fcc7f33f7c86f5786364bd
Nicolas Bremme <nicolas.bremme@isoluce.net>
2021-04-01 14:26:45 +0200

    add

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const VERIFY_TOKEN = "EAAGCK9WZBPQoBAFtfBeE2c0AaEBZBXiDVx2QIURpDtlgm2aotslZApzOmyHpxo1w2tMTXGyPeAQ7id1BOoVxulnaivH4QN7aS5sj3p2Q8FUIobUQlZBODdkZADTZB4Xj1fBYqvChZCtdc6M77a82A619ZBea1dPmqFNJRYmKJ3YnQQZDZD",
-    appUrl = "https://test--chatbot.herokuapp.com";
+    appUrl = "https://test--chatbot.herokuapp.com",
+    pathToFiles = "/app/";
 const kuratorUrl = "https://app.posteria.fr",
     imageUrl = "http://image-kurator.fr/app";
 const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
+const { response } = require('express');
 const { fstat } = require('fs');
 const { parse } = require('path');
 //  Imports dependencies and set up http server
@@ -24,23 +26,6 @@ app.set('port', port);
 app.listen(port, () => console.log('WEBHOOK_OK'));
 
 const getRandom = (min, max) => (Math.floor(Math.random() * ((max - min) + min)));
-
-//Quand le tweet est bon
-var rewardsUrlOk = [
-    "Génial ce tweet ! \u{1F609}",
-    "Super tweet ! \u{1F929}",
-    "Beau travail !  \u{1F642}",
-	"Well done ! \u{1F917}",
-	"Very well \u{1F618}",
-	"Very good \u{1F44D}",
-	"Bravo \u{1F44F}",
-	"Tweet validé \u{1F642}",
-	"Good work \u{1F44A}",
-	"Tweet ok \u{1F3FE}",
-	"Ton tweet est excellent \u{1F60E}",
-	"Géniiiiial !!!!! \u{1F60D}",
-	"Bon boulot !!! \u{1F601}"
-];
 
 //Quand catégories ok
 var rewardsCategoriesOk = [
@@ -123,8 +108,11 @@ app.post('/webhook/', function (req, res)
                 time: "",
             };
         }
-    
-        if (event.message && event.message.text) {
+
+        if (event.message && event.message.quick_reply) {
+            doPostback(allUsers[sender], event);
+        }
+        else if (event.message && event.message.text) {
             doMessage(allUsers[sender], event);
         }
         else if (event.message && event.message.attachments) {
@@ -139,13 +127,6 @@ app.post('/webhook/', function (req, res)
                 console.log('attachment is an image');
             }
         }
-        else if (event.postback && event.postback.payload) {
-            doPostback(allUsers[sender], event);
-        }
-        else if (event.account_linking) {
-            doLinking(allUsers[sender], event);
-        }
-
     }
     res.sendStatus(200)
 });
@@ -168,6 +149,59 @@ app.get('/webhook/', (req, res) => {
     }
 });
 
+app.get('/loginPosteria/', (req, res) => {
+
+    let code = null;
+    let user = null;
+    let sender = null;
+
+    if (null != req.query.code) {
+        code = parseInt(req.query.code, 10);
+    }
+    if (null != req.query.sender) {
+        sender = parseInt(req.query.sender, 10);
+        user = allUsers[sender];
+    }
+    if (user != null && user.isConnected == 0) {
+        if (code == 1 && sender != null) {
+            user.isConnected = 1;
+            kuratorRequest('/api/getCategoriesAndAuthors', {extern_id: user.sender}, function(err, res, body) {
+                try {
+                    body = JSON.parse(body);
+                    let sender = parseInt(body.sender);
+                    
+                    allUsers[sender].platform = body.platform;
+
+                    for (const property in body.categories) {
+                        allUsers[sender].allCategories.push(property);
+                        allUsers[sender].allCategoriesId.push(body.categories[property]);
+                    }
+
+                    if (allUsers[sender].platform == 'wordpress') {
+                        for (const property in body.authors) {
+                            allUsers[sender].allAuthors.push(body.authors[property].username);
+                            allUsers[sender].allAuthorsId.push(property);
+                        }
+                    }
+                    QR_askCategories(allUsers[sender], 1);
+                }
+                catch (error) {
+                    console.log('[1] ' + error);
+                    sendTextMessage(allUsers[sender], {text: "Une erreur s'est produite. [2]"});
+                    delete allUsers[sender];
+                    return;
+                }
+            });
+        } else {
+            sendTextMessage(user, {text: 'Impossible de vous connecter à Kurator.'});
+            delete allUsers[sender];
+        }
+    } else {
+        return;
+    }
+    res.sendFile(pathToFiles + 'loginPosteria.html');
+});
+
 function doMessage(user, event)
 {
     let message = event.message.text;
@@ -177,14 +211,15 @@ function doMessage(user, event)
         return;
     }
     if (user.urlEntered == 0) {
-        checkURL(user, message);
-        //user.skip = 1;
+        if (message.search(kuratorUrl) == -1) {
+            checkURL(user, message);
+        }
         return;
     }
     if (user.categoriesSelected == 1 && user.descLong.length == 0) {
         user.descLong = message;
         if (user.platform == 'wordpress') {
-            askAuthor(user);
+            QR_askAuthor(user);
         } else {
             showPostInfo(user);
         }
@@ -194,7 +229,7 @@ function doMessage(user, event)
 
 function doPostback(user, event)
 {
-    let payload = event.postback.payload;
+    let payload = event.message.quick_reply.payload;
 
     if (user.categoriesSelected == 0) {
         if (payload == "send" && user.categories.length != 0) {
@@ -202,7 +237,7 @@ function doPostback(user, event)
             askLong(user);
             return;
         }
-        else {
+        else if (payload != "send") {
             let newCategorie = 1;
 
             for (let i = 0; i < user.categories.length; i++) {
@@ -214,6 +249,11 @@ function doPostback(user, event)
             if (newCategorie == 1) {
                 user.categories.push(user.allCategoriesId[parseInt(payload)]);
             }
+            QR_askCategories(user, 0);
+            return;
+        }
+        else {
+            QR_askCategories(user, 2);
             return;
         }
     }
@@ -254,7 +294,9 @@ function doPostback(user, event)
                         delete allUsers[sender];
                         return;
                     }
-                } catch {
+                }
+                catch (error) {
+                    console.log('[2] ' + error);
                     console.log("Une erreur s'est produite lors de l'enregistrement de l'article");
                     delete allUsers[sender];
                     return;
@@ -265,59 +307,17 @@ function doPostback(user, event)
     }
 }
 
-function doLinking(user, event)
-{
-    let linking = event.account_linking;
-
-    if (user.isConnected == 0) {
-        if (linking.status == 'linked') {
-            user.isConnected = 1;
-            kuratorRequest('/api/getCategoriesAndAuthors', {extern_id: user.sender}, function(err, res, body) {
-                try {
-                    body = JSON.parse(body);
-                    let sender = parseInt(body.sender);
-
-                    allUsers[sender].platform = body.platform;
-                    for (const property in body.categories) {
-                        allUsers[sender].allCategories.push(property);
-                        allUsers[sender].allCategoriesId.push(body.categories[property]);
-                    }
-                    if (allUsers[sender].platform == 'wordpress') {
-                        for (const property in body.authors) {
-                            allUsers[sender].allAuthors.push(body.authors[property].username);
-                            allUsers[sender].allAuthorsId.push(property);
-                        }
-                    }
-                    askCategories(allUsers[sender]);
-                }
-                catch {
-                    sendTextMessage(allUsers[sender], {text: "Une erreur s'est produite. [2]"});
-                    delete allUsers[sender];
-                    return;
-                }
-            });
-        } else {
-            sendTextMessage(user, {text: 'Impossible de vous connecter à Kurator.'});
-            delete allUsers[sender];
-        }
-    }
-}
-
-function askTime(user)
+function QR_askTime(user)
 {
     const btnData = {
-        "type": "template",
-        "payload": {
-            "template_type": "button",
-            "text": "Choisissez le moment de publication :",
-            "buttons": [
-                {"type": "postback", "title": "Immédiatement", "payload": "now"},
-                {"type": "postback", "title": "Dans le tunnel de publication", "payload": "tunnel"},
-                {"type": "postback", "title": "Annulation", "payload": "stop"}
-            ]
-        }
+        "text": "Choisissez un moment du publication :",
+        "quick_replies": [
+            {"content_type": "text", "title": "Immédiatement", "payload": "now"},
+            {"content_type": "text", "title": "Dans le tunnel de publication", "payload": "tunnel"},
+            {"content_type": "text", "title": "Annulation", "payload": "stop"}
+        ]
     };
-    createBtn(user, btnData);
+    sendTextMessage(user, btnData);
 }
 
 function showPostInfo(user)
@@ -341,30 +341,18 @@ function showPostInfo(user)
     sendTextMessage(user, showInfoText, index, indexLimit, sendTextMessage);
 }
 
-function askAuthor(user)
+function QR_askAuthor(user)
 {
-    let btnCount = Math.ceil(user.allAuthors.length / 3);
-    let btnData = [];
+    let btnData = {
+        "text": "Choisissez un auteur :",
+        "quick_replies": []
+    };
+    let buttons = btnData.quick_replies;
 
-    for (let i = 0, j = 0; i < btnCount; i++) {
-        btnData.push({
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": (i == 0) ? "Choisissez un auteur :" : "Suite :",
-                "buttons": []
-            }
-        });
-        for (j = 0; j < 3 && user.allAuthors[(i * 3) + j]; j++) {
-            let buttons = btnData[i].payload.buttons;
-
-            buttons.push({"type": "postback", "title": user.allAuthors[(i * 3) + j], "payload": (i * 3) + j});
-        }
+    for (let j = 0; j < 13 && user.allAuthors[j]; j++) {
+        buttons.push({"content_type": "text", "title": user.allAuthors[j], "payload": j});
     }
-    let index = 0;
-    let indexLimit = btnData.length - 1;
-
-    createBtn(user, btnData, index, indexLimit, createBtn);
+    sendTextMessage(user, btnData);
 }
 
 function askLong(user)
@@ -375,43 +363,29 @@ function askLong(user)
     sendTextMessage(user, textDescLong);
 }
 
-function askCategories(user)
+function QR_askCategories(user, mode)
 {
-    let btnCount = Math.ceil(user.allCategories.length / 3);
-    let btnData = [];
+    let btnData = {
+        "text": "",
+        "quick_replies": []
+    };
+    let buttons = btnData.quick_replies;
 
-    for (let i = 0, j = 0; i < btnCount; i++) {
-        btnData.push({
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "",
-                "buttons": []
-            }
-        });
-        btnData[i].payload.text = (i == 0) ? "Choisissez une ou plusieurs catégorie(s) :" : "Suite :";
-        for (j = 0; j < 3 && user.allCategories[(i * 3) + j]; j++) {
-            let buttons = btnData[i].payload.buttons;
-
-            buttons.push({"type": "postback", "title": "", "payload": ""});
-            buttons[j].title = user.allCategories[(i * 3) + j];
-            buttons[j].payload = (i * 3) + j;
-        }
+    if (mode == 0) {
+        btnData.text = "Ensuite ?";
     }
-    let index = 0;
-    let indexLimit = btnData.length;
+    else if (mode == 1) {
+        btnData.text = "Choisissez une ou plusieurs catégorie(s) et appuyez sur Send :";
+    }
+    else if (mode == 2) {
+        btnData.text = "Vous devez sélectionner au moins 1 catégorie.";
+    }
 
-    btnData.push({
-        "type": "template",
-        "payload": {
-            "template_type": "button",
-            "text": "Quand vous avez sélectionné toute les catégories, appuyez sur le bouton \"send\":",
-            "buttons": [
-                {"type": "postback", "title": "Send", "payload": "send"},
-            ]
-        }
-    });
-    createBtn(user, btnData, index, indexLimit, createBtn);
+    for (let j = 0; j < 12 && user.allCategories[j]; j++) {
+        buttons.push({"content_type": "text", "title": user.allCategories[j], "payload": j});
+    }
+    buttons.push({"content_type": "text", "title": "Send", "payload": "send"});
+    sendTextMessage(user, btnData);
 }
 
 function kuratorRequest(uri, param, callback)
@@ -452,16 +426,10 @@ function checkURL(user, text)
                     allUsers[sender].image = body.image;
                     allUsers[sender].title = body.title;
                     allUsers[sender].desc = body.description;
-                    createBtn(allUsers[sender], {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "button",
-                            "text": rewardsUrlOk[getRandom(0, rewardsUrlOk.length)] + " Veuillez vous connecter à Kurator :",
-                            "buttons": [
-                                {"type": "account_link", "url": kuratorUrl + '?extern_id=' + sender},
-                            ]
-                        }
-                    });
+                    sendTextMessage(allUsers[sender], [
+                        {attachment: {type: "image", payload: {url: kuratorUrl + "/img/posteria/kurator_no_gbest-publication.jpg"}}},
+                        {text: kuratorUrl + '?extern_id=' + sender}
+                    ], 0, 1, sendTextMessage);
                 }
                 else {
                     if(body.error == 'Cannot parse the article.') {
@@ -471,7 +439,9 @@ function checkURL(user, text)
                     }
                     delete allUsers[sender];
                 }
-            } catch {
+            }
+            catch (error) {
+                console.log('[3] ' + error);
                 sendTextMessage(allUsers[sender], {text: "Une erreur s'est produite. [1]"});
                 delete allUsers[sender];
                 return;
@@ -480,29 +450,6 @@ function checkURL(user, text)
     } else {
         console.log('Not an URI');
     }
-}
-
-function createBtn(user, btnData, index, indexLimit, callback)
-{
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: VERIFY_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: user.sender},
-            message: {attachment: (index != undefined) ? btnData[index] : btnData}
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error creating button: ', error);
-        }
-        else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-        if (callback != undefined && index < indexLimit) {
-            callback(user, btnData, index + 1, indexLimit, callback);
-        }
-    });
 }
 
 function sendTextMessage(user, msgData, index, indexLimit, callback)
@@ -520,13 +467,13 @@ function sendTextMessage(user, msgData, index, indexLimit, callback)
             console.log('Error sending message: ', error);
         }
         else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+            console.log('[4]Error: ', response.body.error);
         }
         if (callback != undefined && index < indexLimit) {
             callback(user, msgData, index + 1, indexLimit, callback);
         }
         else if ((user.platform == 'wall' || user.author.length != 0) && user.time.length == 0 && index >= indexLimit) {
-            askTime(user);
+            QR_askTime(user);
         }
     });
 }
