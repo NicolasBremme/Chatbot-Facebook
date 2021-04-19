e89f24efcc99a43a5453062b7cd7d630c9d67cbe
Nicolas Bremme <nicolas.bremme@isoluce.net>
2021-04-19 12:57:55 +0200

    add

--- index.js
+++ index.js
@@ -434,7 +434,7 @@ function checkURL(user, text)
                     allUsers[sender].desc = body.description;
                     kuratorRequest('/users/login', {extern_id: sender, autologin: 1}, function(err, res, body) {
                         try {
-                            console.log(body);
+                            console.log(res.body);
                             body = JSON.parse(body);
                             let sender = parseInt(body.sender, 10);
                             let code = parseInt(body.code, 10);
