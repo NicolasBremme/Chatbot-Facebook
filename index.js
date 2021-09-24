49889f3f91ccde53efa8eeaad7d675506716dd47
Nicolasata <nicolas.atanasyan@gmail.com>
2021-09-24 16:01:07 +0200

    add

--- index.js
+++ index.js
@@ -1016,6 +1016,8 @@ function getSelectedTime(user, event)
             actionUrl = 'addMediasChatbot';
         }
 
+        console.log('actionUrl', actionUrl);
+
         posteriaRequest('/api/'+actionUrl, postInfos, function(err, res, body) {
 
             let sender = null;
